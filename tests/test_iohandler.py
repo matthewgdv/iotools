91bd38c858c8f122f4f9300b2a1f53dc0750dc58
matthewgdv <matthewgdv@gmail.com>
2019-07-06 20:19:24 +0100

    Add empty unit tests

---
+++ tests/test_iohandler.py
@@ -0,0 +1,141 @@
+import pytest
+
+
+def test_ArgParser___init__():
+    assert True
+
+
+def test_ArgParser__get_formatter():
+    assert True
+
+
+def test_ArgParser_format_help():
+    assert True
+
+
+def test_ArgParser_format_usage():
+    assert True
+
+
+def test_ArgsCommandLineParser___init__():
+    assert True
+
+
+def test_Argument___init__():
+    assert True
+
+
+def test_Argument___repr__():
+    assert True
+
+
+def test_Argument___str__():
+    assert True
+
+
+def test_Argument__validate_and_coerce_argtype():
+    assert True
+
+
+def test_Argument__validate_and_set():
+    assert True
+
+
+def test_Argument__validate_choices():
+    assert True
+
+
+def test_Argument__validate_condition():
+    assert True
+
+
+def test_Argument__validate_nullable():
+    assert True
+
+
+def test_Condition___call__():
+    assert True
+
+
+def test_Condition___init__():
+    assert True
+
+
+def test_Condition___repr__():
+    assert True
+
+
+def test_IOHandler___getitem__():
+    assert True
+
+
+def test_IOHandler___init__():
+    assert True
+
+
+def test_IOHandler___repr__():
+    assert True
+
+
+def test_IOHandler__determine_shortform_alias():
+    assert True
+
+
+def test_IOHandler__generate_attributes_from_args():
+    assert True
+
+
+def test_IOHandler__load_latest_input_config():
+    assert True
+
+
+def test_IOHandler__run_as_gui():
+    assert True
+
+
+def test_IOHandler__run_from_commandline():
+    assert True
+
+
+def test_IOHandler__run_programatically():
+    assert True
+
+
+def test_IOHandler__save_latest_input_config():
+    assert True
+
+
+def test_IOHandler__set_arguments_directly():
+    assert True
+
+
+def test_IOHandler__set_new_argument_defaults():
+    assert True
+
+
+def test_IOHandler__validate_arg_name():
+    assert True
+
+
+def test_IOHandler__workfolder_setup():
+    assert True
+
+
+def test_IOHandler__write_input_config_to_log():
+    assert True
+
+
+def test_IOHandler_add_arg():
+    assert True
+
+
+def test_IOHandler_clear_output():
+    assert True
+
+
+def test_IOHandler_collect_input():
+    assert True
+
+
+def test_IOHandler_show_output():
+    assert True
