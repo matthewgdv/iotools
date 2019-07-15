79651f791f1e08c20c6e42d22b501a88b7f9bcec
matthewgdv <matthewgdv@gmail.com>
2019-07-15 14:55:00 +0100

    Update README.md

--- iotools/widget/__init__.py
+++ iotools/widget/__init__.py
@@ -1,5 +1,5 @@
 __all__ = [
-    "WidgetManager", "Label", "Button", "Checkbox", "CheckBar", "DropDown", "Entry", "Text", "FileSelect", "DirSelect", "Calendar", "DateTimeEdit", "HtmlDisplay", "ProgressBar", "FileSelect", "Table", "ListTable", "DictTable"
+    "WidgetManager", "Label", "Button", "Checkbox", "CheckBar", "DropDown", "Entry", "Text", "FileSelect", "DirSelect", "Calendar", "DateTimeEdit", "HtmlDisplay", "ProgressBar", "Table", "ListTable", "DictTable"
 ]
 
 from .widget import WidgetManager, Label, Button, Checkbox, CheckBar, DropDown, Entry, Text, FileSelect, DirSelect, Calendar, DateTimeEdit, HtmlDisplay, ProgressBar, Table, ListTable, DictTable
