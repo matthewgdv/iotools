0b1696d38516b17d233c4c2acb9f4d717bf63ce8
matthewgdv <matthewgdv@gmail.com>
2019-12-16 10:01:44 +0000

    Fix incorrect datetime format for filename of logs

--- iotools/misc/log.py
+++ iotools/misc/log.py
@@ -84,7 +84,7 @@ class Log:
         """Create a new Log from the given arguments."""
         default_log_dir = Dir.from_home().d.documents.new_dir("Python").new_dir("logs")
         logdir = Dir.from_pathlike(Maybe(log_dir).else_(default_log_dir))
-        path = fR"{logdir}{os.sep}{log_name}Log{DateTime.now().to_logformat()}{file_extension if file_extension.startswith('.') else f'.{file_extension}'}"
+        path = fR"{logdir}{os.sep}{log_name}Log{DateTime.now().to_filetag()}{file_extension if file_extension.startswith('.') else f'.{file_extension}'}"
 
         return cls(path, active=active)
 
